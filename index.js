38db3b2463f3f2611f6cd608f55b31ac1316cafa
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 02:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 drivers=["Larry","Moe","Curly","Shemp","Joe"];
 
 function findMatching(name){
-  drivers.filter(function (drv){drv===name});
+  drivers.filter(function (drv){return drv===name});
 }
